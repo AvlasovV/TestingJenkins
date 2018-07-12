fa40ee176a88a409218d2fff816fbd6a61aabb88
AulasauU <avlasov.vladislav@yandex.by>
2018-07-12 12:38:42 +0300

    Added parameter input through cmd

--- venv/AccessToIBAPortal.py
+++ venv/AccessToIBAPortal.py
@@ -1,4 +1,6 @@
-import pytest
+# import pytest
+
+import argparse
 import time
 import selenium.common
 from selenium import webdriver
@@ -7,26 +9,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-# def test_simple_math():
-#     p = 3 * 6
-#     b = 6 * 4
-#     assert p == b
-# test_simple_math()
 
+# parser = argparse.ArgumentParser(description="Just parameters for cmd line")
+# parser.add_argument('login')
 #just for security add txt-file and we shouldn't push it into the repo
-with open(r"D:\Aulasau_U\PycharmProjects\TasksForStudy\TryingSelenium\venv\password.txt", "r") as file:
-    array = [row.strip() for row in file]
-login = array[0]
-password = array[1]
+# with open(r"D:\Aulasau_U\PycharmProjects\TasksForStudy\TryingSelenium\venv\password.txt", "r") as file:
+#     array = [row.strip() for row in file]
+# login = array[0]
+# password = array[1]
+#
+# NAME = "Aulasau"
 
-NAME = "Aulasau"
+parser = argparse.ArgumentParser(description="Just parameters for cmd line")
+parser.add_argument('login', type=str)
+parser.add_argument('password', type=str)
+parser.add_argument('searchName', type=str)
+
+args = parser.parse_args()
+login = args.login
+password = args.password
+name = args.searchName
 
 
-def test_checking_name(name):
-    elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[1]/div/input")
-    elem.send_keys(name)
-    elem.click()
-    assert driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[3]/table").is_enabled()
+# def test_checking_name(name):
+#     elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[1]/div/input")
+#     elem.send_keys(name)
+#     elem.click()
+#     time.sleep(1)
+#     assert driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[3]/table").is_enabled()
     # try:
     #     if driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[3]/table").is_enabled():
     #         print("We found something")
@@ -51,7 +61,7 @@ try:
     elem.send_keys(password)
     elem.submit()
 
-    time.sleep(3)
+    time.sleep(5)
 
     elem = driver.find_element_by_xpath(".//*[@id='banner']/div/div[2]/div/div[2]/ul/li[2]/a")
     elem.click()
@@ -59,16 +69,18 @@ try:
     elem.click()
 
 
-    test_checking_name(NAME)
-    # elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[1]/div/input")
-    # elem.send_keys(NAME)
-    # elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[3]/button")
+    elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[1]/div/input")
+    elem.send_keys(name)
+    elem = driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[1]/div[3]/button")
     # elem.click()
-    # try:
-    #     if driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[3]/table").is_enabled():
-    #         print("We found something")
-    # except(selenium.common.exceptions.NoSuchElementException):
-    #     print("No results found")
+    # element = driver.find_element_by_xpath("(//DIV[@class='col-md-4'])[3]//BUTTON[@type='button'][text()='SEARCH']")
+    driver.execute_script("arguments[0].click();", elem)
+
+    try:
+        if driver.find_element_by_xpath(".//*[@id='_SearchPerson_INSTANCE_6NvxyekxSIB6_']/div/div[3]/table").is_enabled():
+            print("We found something")
+    except(selenium.common.exceptions.NoSuchElementException):
+        print("No results found")
 
     print("ending test")
 finally:
